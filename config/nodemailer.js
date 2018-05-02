a2d94c0c0bc9c6431d188c1c34e0b9126dc04da4
Anthony <contact@anthonymayfield.com>
2018-05-02 16:03:17 -0700

    refactor forgot endpoint

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,8 +1,9 @@
 const nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
 
 process.on('unhandledRejection', console.log);
 
-var transporter = nodemailer.createTransport({
+const zohoTransport = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
   port: process.env.EMAIL_SMTP_PORT,
   secure: true, // use SSL
@@ -12,4 +13,16 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-module.exports = transporter;
+// console.log(process.env.MAILGUN_API_KEY)
+
+const mailgunTransport = nodemailer.createTransport(mg({
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: 'sandbox1976f4ad59764de18b0ba4d0040a471c.mailgun.org'
+  }
+}));
+
+module.exports = {
+  mailgunTransport,
+  zohoTransport
+};
