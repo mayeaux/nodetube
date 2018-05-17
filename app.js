ee68d24e9344315b2dbe7430e5d89985f547d83a
Anthony <contact@anthonymayfield.com>
2018-05-17 12:34:59 -0700

    dont show log location

--- app.js
+++ app.js
@@ -35,19 +35,19 @@ const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
 
-/** Code to find errant console logs **/
-['log', 'warn', 'error'].forEach(function(method) {
-  var old = console[method];
-  console[method] = function() {
-    var stack = (new Error()).stack.split(/\n/);
-    // Chrome includes a single "Error" line, FF doesn't.
-    if (stack[0].indexOf('Error') === 0) {
-      stack = stack.slice(1);
-    }
-    var args = [].slice.apply(arguments).concat([stack[1].trim()]);
-    return old.apply(console, args);
-  };
-});
+// /** Code to find errant console logs **/
+// ['log', 'warn', 'error'].forEach(function(method) {
+//   var old = console[method];
+//   console[method] = function() {
+//     var stack = (new Error()).stack.split(/\n/);
+//     // Chrome includes a single "Error" line, FF doesn't.
+//     if (stack[0].indexOf('Error') === 0) {
+//       stack = stack.slice(1);
+//     }
+//     var args = [].slice.apply(arguments).concat([stack[1].trim()]);
+//     return old.apply(console, args);
+//   };
+// });
 
 
 /** Load environment variables from .env file, where API keys and passwords are configured. **/
