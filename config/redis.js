29e6ab6bf1608de8f5e03cefcb1042389e06ebce
Anthony <contact@anthonymayfield.com>
2019-12-29 13:14:44 -0700

    try redis fix

--- config/redis.js
+++ config/redis.js
@@ -1,20 +1,24 @@
 const Promise = require('bluebird');
 var redis = require('redis');
 
+if (process.env.REDIS_URL){
+  var client = redis.createClient(process.env.REDIS_URL); //creates a new redisClient
+} else {
+  const redisHost = process.env.REDIS_HOST || '127.0.0.1';
+  const redisPort = process.env.REDIS_PORT || 6379;
+  const redisPassword =  process.env.REDIS_PASSWORD || '';
 
-const redisHost = process.env.REDIS_HOST || '127.0.0.1';
-const redisPort = process.env.REDIS_PORT || 6379;
-const redisPassword =  process.env.REDIS_PASSWORD || '';
+  const options = {
+    host: redisHost,
+    port: redisPort,
+    password: redisPassword
+  };
 
-const options = {
-  host: redisHost,
-  port: redisPort,
-  password: redisPassword
-};
+  console.log(`Connecting to redis, host: ${redisHost}, port: ${redisPort}`)
 
-console.log(`Connecting to redis, host: ${redisHost}, port: ${redisPort}`)
+  var client = redis.createClient(options); //creates a new redisClient
 
-var client = redis.createClient(options); //creates a new redisClient
+}
 
 Promise.promisifyAll(redis.RedisClient.prototype);
 Promise.promisifyAll(redis.Multi.prototype);
