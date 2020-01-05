51a39bc0545bff649e614c250c30491f6e589f68
DMDXV <Padfootdiamond@Gmail.com>
2020-01-05 02:23:24 +0000

    More frontend improvements

--- controllers/frontend/mediaBrowsing.js
+++ controllers/frontend/mediaBrowsing.js
@@ -1,32 +1,33 @@
-const _ = require('lodash');
+const _ = require("lodash");
 
-const redisClient = require('../../config/redis');
+const redisClient = require("../../config/redis");
 
-const Promise = require('bluebird');
+const Promise = require("bluebird");
 
-const pagination = require('../../lib/helpers/pagination');
+const pagination = require("../../lib/helpers/pagination");
 
-const User = require('../../models/index').User;
-const Upload = require('../../models/index').Upload;
-const SearchQuery = require('../../models/index').SearchQuery;
-const View = require('../../models/index').View;
+const User = require("../../models/index").User;
+const Upload = require("../../models/index").Upload;
+const SearchQuery = require("../../models/index").SearchQuery;
+const View = require("../../models/index").View;
 
-
-const uploadHelpers = require('../../lib/helpers/settings');
+const uploadHelpers = require("../../lib/helpers/settings");
 
 const uploadServer = uploadHelpers.uploadServer;
 
-const getFromCache = require('../../caching/getFromCache');
+const getFromCache = require("../../caching/getFromCache");
 
-const uploadFilters = require('../../lib/mediaBrowsing/helpers');
+const uploadFilters = require("../../lib/mediaBrowsing/helpers");
 
-const getSensitivityFilter =  uploadFilters.getSensitivityFilter;
+const getSensitivityFilter = uploadFilters.getSensitivityFilter;
 
-const categories = require('../../config/categories');
+const categories = require("../../config/categories");
 
 const logCaching = process.env.LOG_CACHING;
 
-console.log('UPLOAD SERVER: ' + uploadServer + ' on: media browsing frontend controller');
+console.log(
+  "UPLOAD SERVER: " + uploadServer + " on: media browsing frontend controller"
+);
 
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
@@ -34,63 +35,56 @@ function getParameterByName(name, url) {
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
     results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return '';
+  if (!results[2]) return "";
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-const mongooseHelpers = require('../../caching/mongooseHelpers');
-
+const mongooseHelpers = require("../../caching/mongooseHelpers");
 
 // todo: get out of controller
 let viewStats;
 let indexResponse = {};
-async function getStats(){
-  let views = await redisClient.getAsync('dailyStatsViews');
+async function getStats() {
+  let views = await redisClient.getAsync("dailyStatsViews");
   viewStats = JSON.parse(views);
-
 }
 
-if(!process.env.FILE_HOST  || process.env.FILE_HOST == 'false') {
+if (!process.env.FILE_HOST || process.env.FILE_HOST == "false") {
   getStats();
-  setInterval(function () {
-    getStats()
+  setInterval(function() {
+    getStats();
   }, 1000 * 60 * 1);
 
-
-  async function setIndex(){
-    indexResponse = await redisClient.hgetallAsync('indexValues');
-    if(logCaching == 'true'){
-      console.log('got index cache');
+  async function setIndex() {
+    indexResponse = await redisClient.hgetallAsync("indexValues");
+    if (logCaching == "true") {
+      console.log("got index cache");
     }
   }
 
   setIndex();
 
-  setInterval(function(){
-    setIndex()
+  setInterval(function() {
+    setIndex();
   }, 1000 * 60 * 2);
 }
-
-
 
 /**
  * GET /media/recent
  * Page displaying most recently uploaded content
  */
 exports.recentUploads = async (req, res) => {
-
   try {
+    console.log("getting recent uploads");
 
-    console.log('getting recent uploads');
-
-    const addressPrepend = '/media/recent';
+    const addressPrepend = "/media/recent";
 
     // get media page, either video, image, audio or all
-    let media = req.query.media || 'all';
+    let media = req.query.media || "all";
 
-    let category = req.query.category || '';
+    let category = req.query.category || "";
 
-    let subcategory = req.query.subcategory || '';
+    let subcategory = req.query.subcategory || "";
 
     // get current page
     let page = parseInt(req.params.page || 1);
@@ -98,11 +92,11 @@ exports.recentUploads = async (req, res) => {
     // limit amount to list per page
     let limit = 102;
 
-    if(!category){
-      limit = 6
+    if (!category) {
+      limit = 6;
     }
 
-    const skipAmount = (page * limit) - limit;
+    const skipAmount = page * limit - limit;
 
     // get numbers for pagination
     const startingNumber = pagination.getMiddleNumber(page);
@@ -113,20 +107,30 @@ exports.recentUploads = async (req, res) => {
     let filter = getSensitivityFilter(req.user, req.siteVisitor);
 
     let categoryObj;
-    for(const cat of categories) {
+    for (const cat of categories) {
       if (cat.name == category) {
-        categoryObj = cat
+        categoryObj = cat;
       }
     }
 
     const mediaType = media;
 
-    const uploads = await getFromCache.getRecentUploads(limit, skipAmount, mediaType, filter, category, subcategory);
+    const uploads = await getFromCache.getRecentUploads(
+      limit,
+      skipAmount,
+      mediaType,
+      filter,
+      category,
+      subcategory
+    );
 
-    console.log('rendering')
+    const recentPopular = "recent";
 
-    res.render('mediaBrowsing/recentUploads', {
-      title: 'Recent Uploads',
+    console.log("rendering");
+
+    res.render("mediaBrowsing/recentUploads", {
+      title: "Recent Uploads",
+      recentPopular,
       uploads,
       numbersArray,
       highlightedNumber: page,
@@ -137,61 +141,54 @@ exports.recentUploads = async (req, res) => {
       siteVisitor: req.siteVisitor,
       categories,
       category,
-      isACategory : category,
+      isACategory: category,
       addressPrepend,
       categoryObj
     });
-
-  } catch (err){
+  } catch (err) {
     console.log(err);
     res.status(500);
-    res.send('error')
+    res.send("error");
   }
-
 };
-
-
-
 
 /**
  * GET /media/popular
  * Page with all popular
  */
 exports.popularUploads = async (req, res) => {
+  console.log("getting popular uploads");
 
-  console.log('getting popular uploads');
-
-  const addressPrepend = '/media/popular';
+  const addressPrepend = "/media/popular";
 
   // get media page, either video, image, audio or all
-  let media = req.query.media || 'all';
+  let media = req.query.media || "all";
 
-  let category = req.query.category || '';
+  let category = req.query.category || "";
 
-  let subcategory = req.query.subcategory || '';
+  let subcategory = req.query.subcategory || "";
 
   const within = req.query.within;
 
   // setup page
   let page = req.params.page;
-  if(!page){ page = 1 }
+  if (!page) {
+    page = 1;
+  }
   page = parseInt(page);
 
   let limit = 102;
 
-  if(!category){
-    limit = 6
-  };
+  if (!category) {
+    limit = 6;
+  }
 
-
-
-  const skipAmount = (page * limit) - limit;
+  const skipAmount = page * limit - limit;
 
   const startingNumber = pagination.getMiddleNumber(page);
   const numbersArray = pagination.createArray(startingNumber);
   const previousNumber = pagination.getPreviousNumber(page);
   const nextNumber = pagination.getNextNumber(page);
-
 
   const withinString = pagination.createWithinString(req.query.within);
 
@@ -203,8 +200,7 @@ exports.popularUploads = async (req, res) => {
   let viewAmountInPeriod;
 
   try {
-
-    switch(englishString){
+    switch (englishString) {
       case "Last Hour":
         viewAmountInPeriod = viewStats.hour;
         break;
@@ -227,36 +223,46 @@ exports.popularUploads = async (req, res) => {
 
     let filter = getSensitivityFilter(req.user, req.siteVisitor);
 
-    let uploads = await getFromCache.getPopularUploads(timeRange, limit, skipAmount, mediaType, filter, category, subcategory);
+    let uploads = await getFromCache.getPopularUploads(
+      timeRange,
+      limit,
+      skipAmount,
+      mediaType,
+      filter,
+      category,
+      subcategory
+    );
 
     let categoryObj;
-    for(const cat of categories) {
+    for (const cat of categories) {
       if (cat.name == category) {
-        categoryObj = cat
+        categoryObj = cat;
       }
     }
 
-    let withinDisplayString = '';
-    if(within == '1hour'){
-      withinDisplayString = 'last hour'
-    } else if (within == '24hour'){
-      withinDisplayString = 'last 24 hours'
-    } else if (within == '1week'){
-      withinDisplayString = 'last week'
-    } else if (within == '1month') {
-      withinDisplayString = 'last month'
+    let withinDisplayString = "";
+    if (within == "1hour") {
+      withinDisplayString = "last hour";
+    } else if (within == "24hour") {
+      withinDisplayString = "last 24 hours";
+    } else if (within == "1week") {
+      withinDisplayString = "last week";
+    } else if (within == "1month") {
+      withinDisplayString = "last month";
     }
 
-    withinDisplayString = 'views ' + withinDisplayString;
+    withinDisplayString = "views " + withinDisplayString;
 
-    const popularTimeViews = 'viewsWithin' + within;
+    const popularTimeViews = "viewsWithin" + within;
+    const recentPopular = "popular";
 
     console.log(popularTimeViews);
 
-    console.log('getting popular uploads');
+    console.log("getting popular uploads");
 
-    res.render('mediaBrowsing/popularUploads', {
-      title: 'Popular Uploads',
+    res.render("mediaBrowsing/popularUploads", {
+      title: "Popular Uploads",
+      recentPopular,
       uploads,
       numbersArray,
       highlightedNumber: page,
@@ -267,10 +273,10 @@ exports.popularUploads = async (req, res) => {
       viewAmountInPeriod,
       uploadServer,
       filter,
-      siteVisitor : req.siteVisitor,
+      siteVisitor: req.siteVisitor,
       categories,
       category,
-      isACategory : category,
+      isACategory: category,
       media,
       addressPrepend,
       categoryObj,
@@ -278,84 +284,79 @@ exports.popularUploads = async (req, res) => {
       withinDisplayString,
       popularTimeViews
     });
-
-  } catch (err){
-    console.log('ERR:')
-    console.log(err)
+  } catch (err) {
+    console.log("ERR:");
+    console.log(err);
 
     res.status(500);
-    return res.render('error/500', {
-      title: 'Server Error'
+    return res.render("error/500", {
+      title: "Server Error"
     });
   }
-
-
 };
 
-
-
-
-
-async function saveSearchQuery(user, search){
+async function saveSearchQuery(user, search) {
   // note the person searching
-  let searcher = user && user.id || undefined;
+  let searcher = (user && user.id) || undefined;
 
-// create and save search query
+  // create and save search query
   const searchQuery = new SearchQuery({
     searcher: searcher,
     query: search
   });
 
   await searchQuery.save();
-
 }
 
-function getOrderByEnglishString(orderByQuery){
+function getOrderByEnglishString(orderByQuery) {
   let orderBy;
-  if(!orderByQuery){
-    orderBy = 'newToOld'
+  if (!orderByQuery) {
+    orderBy = "newToOld";
   } else {
     orderBy = orderByQuery;
   }
 
-  if(orderBy !== 'popular' && orderBy !== 'newToOld' && orderBy !== 'oldToNew'){
-    console.log('doesnt connect');
-    orderBy = 'newToOld'
+  if (
+    orderBy !== "popular" &&
+    orderBy !== "newToOld" &&
+    orderBy !== "oldToNew"
+  ) {
+    console.log("doesnt connect");
+    orderBy = "newToOld";
   }
 
   let orderByEnglishString;
 
-  if(orderBy == 'oldToNew'){
-    orderByEnglishString = 'Old To New'
+  if (orderBy == "oldToNew") {
+    orderByEnglishString = "Old To New";
   }
 
-  if(orderBy == 'newToOld'){
-    orderByEnglishString = 'New To Old'
+  if (orderBy == "newToOld") {
+    orderByEnglishString = "New To Old";
   }
 
-  if(orderBy == 'popular'){
-    orderByEnglishString = 'Popular'
+  if (orderBy == "popular") {
+    orderByEnglishString = "Popular";
   }
 
-  return orderByEnglishString
-
+  return orderByEnglishString;
 }
-
 
 /**
  * GET /
  * Search page.
  */
 exports.search = async (req, res) => {
-
   // setup page
   let page = req.query.page;
-  if(!page){ page = 1 }
+  if (!page) {
+    page = 1;
+  }
   page = parseInt(page);
 
   let limit = 102;
 
-  const skipAmount = (page * limit) - limit;
+  const skipAmount = page * limit - limit;
 
   const startingNumber = pagination.getMiddleNumber(page);
   const numbersArray = pagination.createArray(startingNumber);
@@ -368,11 +369,11 @@ exports.search = async (req, res) => {
 
   const userSearchQuery = req.query.searchQuery;
 
-  if(!userSearchQuery){
-    return res.render('public/search', {
-      title: 'Search',
-      orderBy: 'newToOld',
-      searchQuery: ''
+  if (!userSearchQuery) {
+    return res.render("public/search", {
+      title: "Search",
+      orderBy: "newToOld",
+      searchQuery: ""
     });
   }
 
@@ -381,59 +382,63 @@ exports.search = async (req, res) => {
   let searchType = req.query.searchType;
   const orderBy = req.query.orderBy;
 
-
   let uploads, users;
   const re = new RegExp(userSearchQuery, "gi");
 
   let totalUploadsAmount;
 
-  if(searchType == 'user'){
+  if (searchType == "user") {
     // channels
     users = await User.find({
-      $or : [ { channelName: re }, { channelUrl: re  } ],
-      status: { $ne: 'restricted'}
-    }).populate('uploads');
+      $or: [{ channelName: re }, { channelUrl: re }],
+      status: { $ne: "restricted" }
+    }).populate("uploads");
 
-    users = _.filter(users, function(user){
-      return user.uploads.length > 0
+    users = _.filter(users, function(user) {
+      return user.uploads.length > 0;
     });
-
-  } else if (searchType == 'upload' || !searchType) {
+  } else if (searchType == "upload" || !searchType) {
     const mediaType = req.query.mediaType;
 
     let searchQuery = {
-      visibility: 'public',
-      title : re,
-      $or : [ { status: 'completed' }, { uploadUrl: { $exists: true } } ]
+      visibility: "public",
+      title: re,
+      $or: [{ status: "completed" }, { uploadUrl: { $exists: true } }]
     };
 
-    if(mediaType && mediaType !== 'all'){
-      searchQuery.fileType = mediaType
+    if (mediaType && mediaType !== "all") {
+      searchQuery.fileType = mediaType;
     }
 
-    let sortObj = '';
-    if(orderBy == 'newToOld'){
+    let sortObj = "";
+    if (orderBy == "newToOld") {
       sortObj = {
         createdAt: -1
-      }
-    } else if (orderBy == 'oldToNew'){
+      };
+    } else if (orderBy == "oldToNew") {
       sortObj = {
         createdAt: 1
-      }
+      };
     }
 
     // uploads
-    uploads = await Upload.find(searchQuery).populate('uploader').sort(sortObj).limit(1000);
+    uploads = await Upload.find(searchQuery)
+      .populate("uploader")
+      .sort(sortObj)
+      .limit(1000);
 
     totalUploadsAmount = uploads.length;
 
     // populate upload.legitViewAmount
     uploads = await Promise.all(
-      uploads.map(async function(upload){
+      uploads.map(async function(upload) {
         upload = upload.toObject();
-        const checkedViews = await View.count({ upload: upload.id, validity: 'real' });
+        const checkedViews = await View.count({
+          upload: upload.id,
+          validity: "real"
+        });
         upload.legitViewAmount = checkedViews;
-        return upload
+        return upload;
       })
     );
 
@@ -441,28 +446,27 @@ exports.search = async (req, res) => {
 
     uploads = uploadFilters.filterUploadsBySensitivity(uploads, filter);
 
-    if(orderBy == 'popular'){
+    if (orderBy == "popular") {
       uploads = uploads.sort(function(a, b) {
         return b.legitViewAmount - a.legitViewAmount;
       });
     }
 
-    const helpers = require('../../lib/mediaBrowsing/helpers');
+    const helpers = require("../../lib/mediaBrowsing/helpers");
 
-    uploads = helpers.trimUploads(uploads, limit, skipAmount)
-
+    uploads = helpers.trimUploads(uploads, limit, skipAmount);
   } else {
     // error
   }
 
   const siteVisitor = req.siteVisitor;
 
-  const media = mediaType || 'all';
+  const media = mediaType || "all";
 
   const orderByEnglishString = getOrderByEnglishString(orderBy);
 
-  return res.render('public/search', {
-    title: 'Search',
+  return res.render("public/search", {
+    title: "Search",
     channels: users,
     uploads,
     siteVisitor,
@@ -482,33 +486,30 @@ exports.search = async (req, res) => {
   });
 };
 
-
-
-
 /** TOTALLY UNFINISHED Organize uploads by the amount of the reacts they've received **/
-exports.popularByReacts = async function (req, res){
+exports.popularByReacts = async function(req, res) {
+  let uploads = await Upload.find({ reacts: { $exists: true } }).populate(
+    "reacts uploader"
+  );
 
-  let uploads = await Upload.find({ reacts: { $exists: true }  }).populate('reacts uploader');
-
-  uploads = _.filter(uploads, function(upload){
-    return upload.reacts.length !== 0
+  uploads = _.filter(uploads, function(upload) {
+    return upload.reacts.length !== 0;
   });
 
   uploads = uploads.sort(function(a, b) {
-    return b.reacts.length - a.reacts.length
+    return b.reacts.length - a.reacts.length;
   });
 
   // console.log(uploads);
 
-  for(upload of uploads){
+  for (upload of uploads) {
     upload.checkedViews = [];
   }
 
   console.log(uploads);
 
-  res.render('public/popularByReacts', {
-    title : 'Popular By Reacts',
+  res.render("public/popularByReacts", {
+    title: "Popular By Reacts",
     uploads
   });
 };
-
