0d1aa915426039c53c3e68d0ecb9485a7712c51c
Anthony <contact@anthonymayfield.com>
2020-01-06 17:55:34 -0700

    change way files are created

---
+++ postinstall.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile('.env.settings.sample', '.env.settings', { flag: "wx" }, (err) => {
+  if (err) throw err;
+  console.log('Settings file created');
+});
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile('.env.private.sample', '.env.private', { flag: "wx" }, (err) => {
+  if (err) throw err;
+  console.log('Private keys file created');
+});
