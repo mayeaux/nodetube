23a6d2e1645948ac246bb97316c8e87d958db93a
Anthony <contact@anthonymayfield.com>
2020-01-19 17:03:16 -0700

    skip categories by default

--- caching/getFromCache.js
+++ caching/getFromCache.js
@@ -158,6 +158,7 @@ async function getRecentUploads(limit, offset, mediaType, filter, category, subc
 
     if(category == 'all'){
 
+      // sort all by createdAt dates (most recent to least recent)
       uploads = uploads.sort(function(a, b){
         return new Date(b.createdAt) - new Date(a.createdAt);
       });
