5f379df589e699b25254a96bce82da38389d6e42
Anthony <contact@anthonymayfield.com>
2020-01-25 17:11:13 -0700

    fixes for meta tags

--- controllers/frontend/mediaPlayer.js
+++ controllers/frontend/mediaPlayer.js
@@ -16,7 +16,7 @@ const uploadHelpers = require('../../lib/helpers/settings');
 
 const categories = require('../../config/categories');
 
-const uploadServer  = uploadHelpers.uploadServer;
+let uploadServer  = uploadHelpers.uploadServer;
 
 const generateComments = require('../../lib/mediaPlayer/generateCommentsObjects');
 const generateReactInfo = require('../../lib/mediaPlayer/generateReactInfo');
@@ -207,15 +207,49 @@ exports.getMedia = async(req, res) => {
 
       // document is fine to be shown publicly
 
+      // TODO: put this in the config file for determining uploadUrl
+      function validURL(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+          '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        return !!pattern.test(str);
+      }
+
+      // if the upload server is a relative path prepend the protocol and host to make it an absolute url
+      if(!validURL(uploadServer)){
+        uploadServer = req.protocol + '://' + req.get('host') + uploadServer
+      }
+
       /** SET META TAGS **/
       res.locals.meta.title = `${upload.title}`;
 
       res.locals.meta.description = upload.description || `Hosted on ${brandName}`;
 
       if(upload.fileType == 'video'){
-        res.locals.meta.image = upload.customThumbnailUrl || upload.thumbnailUrl;
-        res.locals.meta.video = upload.uploadUrl;
+        // set proper thumbnail url
+        if(upload.thumbnail && upload.thumbnails.custom){
+          res.locals.meta.image = `${uploadServer}/${upload.uploader.channelUrl}/${upload.thumbnails.custom}`
+        } else if (upload.thumbnails && upload.thumbnails.generated){
+          res.locals.meta.image = `${uploadServer}/${upload.uploader.channelUrl}/${upload.thumbnails.generated}`
+        } else if (upload.thumbnails && upload.thumbnails.medium){
+          res.locals.meta.image = `${uploadServer}/${upload.uploader.channelUrl}/${upload.thumbnails.medium}`
+        }
+
+        res.locals.meta.video = `${uploadServer}/${upload.uploader.channelUrl}/${upload.uniqueTag}.mp4`;
       }
+
+      // TODO: fix this to use updated paths
+      if(upload.fileType == 'audio'){
+        res.locals.meta.image = upload.customThumbnailUrl || upload.thumbnailUrl;
+      }
+
+      // TODO: implement this
+      // if(upload.fileType == 'image'){
+      //   res.locals.meta.image = upload.customThumbnailUrl || upload.thumbnailUrl;
+      // }
 
       const url = req.protocol + '://' + req.get('host') + req.originalUrl;
 
