19e6ad33b2b0180ac95b6103dfed2a833fd182b7
Anthony <contact@anthonymayfield.com>
2020-01-29 21:00:04 -0700

    add brand email

--- middlewares/custom/customBranding.js
+++ middlewares/custom/customBranding.js
@@ -1,6 +1,7 @@
 async function customBranding(req, res, next){
   res.locals.brandName = process.env.INSTANCE_BRAND_NAME;
   res.locals.instanceDomainName = process.env.INSTANCE_DOMAIN_NAME;
+  res.locals.instanceContactEmail = process.env.INSTANCE_CONTACT_EMAIL;
 
   next();
 }
