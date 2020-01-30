d642857cca2bced87a27a64ef74ca822588db742
Anthony <contact@anthonymayfield.com>
2020-01-29 20:50:34 -0700

    add domain name to .env.private and also res.locals

--- middlewares/custom/customBranding.js
+++ middlewares/custom/customBranding.js
@@ -1,7 +1,6 @@
 async function customBranding(req, res, next){
   res.locals.brandName = process.env.INSTANCE_BRAND_NAME;
-
-  // TODO: we can add other things here as well
+  res.locals.instanceDomainName = process.env.INSTANCE_DOMAIN_NAME;
 
   next();
 }
