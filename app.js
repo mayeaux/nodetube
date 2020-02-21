8b6dd56ef305847b2ebc99a621b57d7b916bb01d
Anthony Ettinger <anthony@chovy.com>
2020-02-21 13:27:38 -0800

    add longer timoeut

--- app.js
+++ app.js
@@ -122,8 +122,9 @@ if(cluster.isMaster){
 
     mongoose.connect(mongoUri, {
       keepAlive: true,
-      reconnectTries: Number.MAX_VALUE,
-      useNewUrlParser: true
+      //reconnectTries: Number.MAX_VALUE,
+      useNewUrlParser: true,
+      connectTimeoutMS: 900*1000
     });
 
     if(process.env.MONGOOSE_DEBUG == 'true' || process.env.MONGOOSE_DEBUG == 'on' || 1 == 2){
