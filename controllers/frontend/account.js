0ad0366059a19f4e92ee56e5645b15f6586137d3
Anthony <contact@anthonymayfield.com>
2020-04-04 13:24:43 -0600

    remove comments

--- controllers/frontend/account.js
+++ controllers/frontend/account.js
@@ -60,24 +60,6 @@ exports.getFileUpload = async(req, res) => {
     await req.user.save();
   }
 
-  // userCanUploadContentOfThisRating(process.env.MAX_RATING_ALLOWED, )
-
-  // let maxRatingAllowed = null;
-  // switch(process.env.MIN_AUDIENCE_TYPE_ALLOWED){
-  // case'SFW':
-  //   maxRatingAllowed = 'allAges';
-  //   break;
-  // case'NSFW':
-  //   maxRatingAllowed = 'mature';
-  //   break;
-  // case'SENS':
-  //   maxRatingAllowed = 'sensitive';
-  //   break;
-  // default:
-  //   maxRatingAllowed = 'allAges';
-  //   break;
-  // }
-
   res.render('uploading', {
     title: 'File Upload',
     uploadUrl,
