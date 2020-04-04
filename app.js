078ad3e7ff804756e94cbca68804e47736aaa132
Anthony <contact@anthonymayfield.com>
2020-04-04 08:04:34 -0600

    add github and wiki to social links

--- app.js
+++ app.js
@@ -390,7 +390,7 @@ if(cluster.isMaster){
 
     /** start server **/
     app.listen(app.get('port'), () => {
-      console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+      console.log('\n %s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
       console.log('  Press CTRL-C to stop\n');
     });
 
