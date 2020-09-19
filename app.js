fa290e7c28721419eed786c7471498833588f76b
Anthony <contact@anthonymayfield.com>
2020-09-18 21:39:47 -0600

    abbreviate numbers

--- app.js
+++ app.js
@@ -300,6 +300,33 @@ if(cluster.isMaster){
       next();
     });
 
+    app.use(async function(req, res, next){
+      var SI_SYMBOL = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
+
+      function abbreviateNumber(number){
+
+        // what tier? (determines SI symbol)
+        var tier = Math.log10(number) / 3 | 0;
+
+        // if zero, we don't need a suffix
+        if(tier == 0)return number;
+
+        // get suffix and determine scale
+        var suffix = SI_SYMBOL[tier];
+        var scale = Math.pow(10, tier * 3);
+
+        // scale the number
+        var scaled = number / scale;
+
+        // format number and add suffix
+        return scaled.toFixed(1) + suffix;
+      }
+
+      res.locals.abbreviateNumber = abbreviateNumber;
+
+      next();
+    });
+
     /** Prevent blocked sitevisitors from accessing site **/
     app.use(async(req, res, next) => {
 
