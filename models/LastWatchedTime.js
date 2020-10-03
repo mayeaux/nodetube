c047feb7b01050f8343ab60beafdf5c9df3b2515
Anthony <contact@anthonymayfield.com>
2020-10-02 22:35:20 -0600

    make images clickable

---
+++ models/LastWatchedTime.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const javascriptTimeAgo = require('javascript-time-ago');
+javascriptTimeAgo.locale(require('javascript-time-ago/locales/en'));
+require('javascript-time-ago/intl-messageformat-global');
+require('intl-messageformat/dist/locale-data/en');
+const timeAgoEnglish = new javascriptTimeAgo('en-US');
+
+const reactSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    index: true
+  },
+  upload: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Upload',
+    index: true
+  },
+  secondsWatched: {
+    type: Number
+  },
+  active: {
+    type: Boolean
+  }
+  // RATHER THAN USE VIEWED-AT TIME WE WILL USE CREATED AT TIME AS A STAND-IN
+},{ timestamps: true,
+  toObject: {
+    virtuals: true
+  },
+  toJSON: {
+    virtuals: true
+  }
+});
+
+reactSchema.virtual('timeAgo').get(function(){
+  return timeAgoEnglish.format( new Date(this.createdAt) );
+});
+
+const React = mongoose.model('React', reactSchema);
+
+module.exports = React;
+
