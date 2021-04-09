7b2d6501e0d4482a37b2cc7310553d7ce1e2dc53
Anthony <contact@anthonymayfield.com>
2021-04-09 23:09:41 +0200

    setup middleware and finish frontend

--- middlewares/shared/stripeSubscriptionChecker.js
+++ middlewares/shared/stripeSubscriptionChecker.js
@@ -1,26 +1,39 @@
 const stripe = require('../../lib/payments/stripe');
 
+// middleware to handle and keep straight Stripe subscriptions and Plus abilities
 async function checkSubscriptionRenewalDates(req, res, next){
+  const date = new Date();
+
   const stripeSubscriptionRenewalDate = req.user && req.user.stripeSubscriptionRenewalDate;
+
+  // if it's set to renew
+  if(stripeSubscriptionRenewalDate){
+    // if the current date is a larger value than the renewal date (aka, it's passed the renewal date time)
+    if(stripeSubscriptionRenewalDate < date){
+      // increment this date by 30 days
+      // req.user.stripeSubscriptionRenewalDate =
+    }
+  }
 
   const stripeSubscriptionCancellationDate = req.user && req.user.stripeSubscriptionCancellationDate;
 
-  // const date = new Date();
-  //
-  // const passedRenewalDate = stripeSubscriptionRenewalDate && date > stripeSubscriptionRenewalDate;
-  //
-  // // check that the subscription is still good and update the date
-  // if(passedRenewalDate){
-  //   const newRenewalDate = await getNewRenewalDate(req.user.stripeSubscriptionId);
-  // }
-  //
-  // const passedCancellationDate = stripeSubscriptionCancellationDate && date > stripeSubscriptionCancellationDate;
-  //
-  // if(passedCancellationDate){
-  //   req.user.stripeSubscriptionCanceled = true;
-  // }
-  //
-  // await req.user.save();
+  // if it's set to cancel
+  if(stripeSubscriptionCancellationDate){
+
+    // if the current date is a larger value than the cancellation date (aka, it's passed the cancellation date time)
+    if(stripeSubscriptionCancellationDate < date){
+
+      // await revokeUserPlusFeatures()
+      //
+      // req.user.stripeSubscriptionCancellationDate = undefined;
+      //
+      // await req.user.save();
+
+    }
+  }
+  
+  // third case = user has plus but not renewal date, cancellation fee etc (maybe I should do this as a migration so I can handle errors as they come through)
+
 
 
 
