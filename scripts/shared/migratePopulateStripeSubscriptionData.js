472633321a79951eec950bbc59f9987d9dea049b
Anthony <contact@anthonymayfield.com>
2021-04-13 20:20:10 +0200

    working on migration

--- scripts/shared/migratePopulateStripeSubscriptionData.js
+++ scripts/shared/migratePopulateStripeSubscriptionData.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Promise = require('bluebird');
+const moment = require('moment');
+
 const subscriptionHelpers = require('../../lib/helpers/subscriptions');
+
 
 process.on('uncaughtException', (err) => {
   console.log('Uncaught Exception: ', err);
@@ -62,30 +65,61 @@ async function main(){
 
   for(const user of users){
 
-    await Promise.delay(delay * counter);
+    const firstDelayAmount = delay * counter
+
+    console.log(`Waiting ${firstDelayAmount/1000} seconds`)
+
+    await Promise.delay(firstDelayAmount);
 
     const secondaryDelay = (delay * counter) + 10000;
 
-    console.log(user.channelUrl);
+    console.log(`User channel url: ${user.channelUrl}`);
 
     counter = counter + 1;
 
     try {
 
+      // get info for the customer
       const response = await stripe.customers.retrieve(user.stripeCustomerId);
 
-      console.log(response);
+      const customerDescription = response.description;
 
+      console.log(`Description: ${customerDescription}`)
+
+      // console.log(response);
+
+      // if there are some subscriptions for the user
       if(response.subscriptions.total_count > 0){
         const subscription = response.subscriptions.data[0];
 
-        console.log(subscription);
+        const createdAtTime = moment.unix(subscription.created).format('dddd, MMMM Do, YYYY h:mm:ss A');
+
+        const currentPeriodStart = subscription.current_period_start;
+
+        const currentPeriodEnd = subscription.current_period_end;
+
+        const cancelledAtDate = subscription.canceled_at;
+
+        console.log(`Created at time: ${createdAtTime}`);
+
+        console.log(`Status: ${subscription.status}`);
+
+        console.log(`Current period start: ${currentPeriodStart}`)
+
+        console.log(`Current period end: ${currentPeriodEnd}`)
+
+        console.log(`Cancelled at date: ${cancelledAtDate}`)
+
+        // console.log(subscription);
 
         const status = subscription.status;
 
-        console.log(secondaryDelay);
+        // console.log(secondaryDelay);
 
-        await Promise.delay(delay * counter);
+        console.log(`Waiting ${secondaryDelay/1000} seconds`)
+
+        // secondary delay before
+        await Promise.delay(secondaryDelay);
 
         const subscriptionId = subscription.id;
 
